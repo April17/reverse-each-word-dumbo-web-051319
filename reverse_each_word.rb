07f0c38310d9da5fead10e2444051d94f3925787
Fanqiang Meng <fanmeng0417@gmail.com>
2019-04-25 23:58:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 require "pry"
 def reverse_each_word(string)
   ary = string.split(" ")
-  ary.collect_with_index do |str,index|
-    ary[index] = str.reverse
+  ary.collect do |str|
+    str.reverse
   end
   return ary.join(" ")
 end
